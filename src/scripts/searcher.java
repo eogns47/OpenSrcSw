61c0a512185dd97aecdd06f1f4d01e176704f37d
eogns47 <eogns47@naver.com>
2022-04-07 12:45:53 +0900

    edit calcsim

--- src/scripts/searcher.java
+++ src/scripts/searcher.java
@@ -30,8 +30,13 @@ public class searcher {
 		this.query=query;
 		
 	}
-
+	
+	public void InnerProduct() throws Exception {
+		
+	}
+	
 	public void calcsim() throws Exception {
+		InnerProduct Q_id = new InnerProduct();
 		
 		File file1 = new File(input_file); //index.post 읽어오기
 		File file2 = new File("./collection.xml"); //collection.xml 읽어오기
